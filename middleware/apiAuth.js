0c7ac3dda84b9b6d55a8661aa9bb7734038a5e2e
bitpassion <asher.hoffman1120@gmail.com>
2023-09-14 13:20:08 +0800

    apiauth-change

--- middleware/apiAuth.js
+++ middleware/apiAuth.js
@@ -7,7 +7,7 @@ function checkOrigin(req, res, next) {
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     } else {
         return res.status(403).json({ error: 'Forbidden' });
     }
